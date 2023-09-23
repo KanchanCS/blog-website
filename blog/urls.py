31337c9f466c2143b21339e4c5c6f844fcd01b5d
KanchanCS <kanchan111yadav@gmail.com>
2023-09-24 04:20:59 +0530

    like-button

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ app_name = "blog"
 urlpatterns = [
     path("", views.index, name="index"),
     path("single/<int:id>/", views.single, name="single"),
+    path("like-<int:post_id>/", views.like, name="like"),
+    path("unlike-<int:post_id>/", views.unlike, name="unlike"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404,redirect
-from .models import Category,Post
+from .models import Category,Post,Like
 
 # Create your views here.
 def index(request):
@@ -9,11 +9,30 @@ def index(request):
 def single(request, id):
     if request.user.is_authenticated:
         post = get_object_or_404(Post, pk=id)
-        context = {"post": post}
+        user_is_liked = Like.objects.filter(
+        post_id=post.id, user=request.user
+        ).exists()
+        liked_count = 0
+        liked_count = Like.objects.filter(post_id=post.id).count()
+        context ={
+            "post": post,
+            "user_is_liked": user_is_liked,
+            "liked_count": liked_count,
+            }
         return render(request, 'single.html', context)
     else:
         return redirect('account:login')
 
+def like(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    Like.objects.create(post_id=post.id, user=request.user)
+    return redirect("blog:single", post.id)
+
+
+def unlike(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    Like.objects.filter(post_id=post.id, user=request.user).delete()
+    return redirect("blog:single", post.id)
 
     
             
